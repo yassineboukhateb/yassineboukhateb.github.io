cd1a2663869d88ae92a3296fc5a44a2123d6cc53
yassineboukhateb <126679902+yassineboukhateb@users.noreply.github.com>
2025-06-28 18:11:46 +0000

    Deploying to gh-pages from @ yassineboukhateb/yassineboukhateb.github.io@25a29b40d25108822d7f02c4194e1b6e853f7a7c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please click on the pdf button to get my CV. Last update on June 28, 2025.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
